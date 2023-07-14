c02f7b78a4b4468022348b26606763d80535e592
LucasDCoelho <darkalp99@gmail.com>
2023-07-14 18:02:41 -0300

    new: Created Header.FC and configured authentication with Instagram

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+export function Header(){
+  return(
+    <>
+      <header  className="bg-slate-700 h-16 flex items-center justify-between px-10">
+        <h1 className="text-slate-100">Niely Moreira</h1>
+
+          <img  className="bg-contain relative border-2 border-slate-100 w-10 h-10 rounded-full" src="https://hips.hearstapps.com/hmg-prod/images/close-up-of-purple-crocus-flowers-united-kingdom-uk-royalty-free-image-1674159456.jpg" alt="LC"/>
+      </header>
+    </>
+  );
+}
--- src/container/routes/HomeContainer.tsx
+++ src/container/routes/HomeContainer.tsx
@@ -1,12 +1,10 @@
+import { Header } from "../../components/Header";
 
 
 export function HomeContainer() {
   return (
     <>
-      <div  className="bg-slate-700">
-        <h1>Niely Moreira</h1>
-      </div>
-
+      <Header/>
     </>
   )
 }
